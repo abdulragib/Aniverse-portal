0b08ac3cde0c58d3ee0df3e580ca880a50e16f5b
Abdul Ragib <abdulragib36@gmail.com>
2025-01-26 21:29:40 +0530

    delete and update episode done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,9 @@ import {
   getDocs,
   query,
   Timestamp,
+  deleteDoc,
+  doc,
+  updateDoc,
 } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import styles from "./style.module.css";
@@ -50,7 +53,8 @@ const Home = () => {
   const [series, setSeries] = useState([]);
   const [selectedSeries, setSelectedSeries] = useState(null);
   const [selectedLanguage, setSelectedLanguage] = useState(""); // Language state
-  const [episodesBySeriesAndLanguage, setEpisodesBySeriesAndLanguage] = useState({});
+  const [episodesBySeriesAndLanguage, setEpisodesBySeriesAndLanguage] =
+    useState({});
 
   const [seriesTitle, setSeriesTitle] = useState("");
   const [seriesDescription, setSeriesDescription] = useState("");
@@ -62,6 +66,7 @@ const Home = () => {
   const [duration, setDuration] = useState("");
   const [seriesMode, setSeriesMode] = useState("");
   const [language, setLanguage] = useState(""); // Language for episodes
+  const [editEpisode, setEditEpisode] = useState(null);
 
   const fetchSeries = async () => {
     const q = query(collection(db, "series"));
@@ -131,7 +136,13 @@ const Home = () => {
 
   const handleCreateEpisode = async (e) => {
     e.preventDefault();
-    if (!selectedSeries || !episodeTitle || !mediaFile || !duration || !language) {
+    if (
+      !selectedSeries ||
+      !episodeTitle ||
+      !mediaFile ||
+      !duration ||
+      !language
+    ) {
       alert("All episode fields are required!");
       return;
     }
@@ -140,7 +151,8 @@ const Home = () => {
     const mediaUrl = await getDownloadURL(mediaRef);
 
     const episodesRef = collection(db, "series", selectedSeries, "episodes");
-    await addDoc(episodesRef, {
+     // Add the episode and retrieve the document reference
+     const docRef = await addDoc(episodesRef, {
       title: episodeTitle,
       duration,
       mediaUrl,
@@ -148,11 +160,81 @@ const Home = () => {
       createdAt: Timestamp.now(),
     });
 
+    // Update the newly created document to include the ID
+    await updateDoc(docRef, { id: docRef.id });
+
     alert("Episode added successfully!");
     setEpisodeTitle("");
     setMediaFile(null);
     setDuration("");
     setLanguage("");
+  };
+
+  const handleDeleteEpisode = async (seriesId, episodeId) => {
+    if (window.confirm("Are you sure you want to delete this episode?")) {
+      try {
+        const episodeRef = doc(db, "series", seriesId, "episodes", episodeId);
+        await deleteDoc(episodeRef);
+        alert("Episode deleted successfully!");
+        fetchSeries(); // Refresh the series list to reflect the deletion
+      } catch (error) {
+        console.error("Error deleting episode:", error);
+        alert("Failed to delete episode.");
+      }
+    }
+  };
+
+  const handleUpdateEpisode = async (e) => {
+    e.preventDefault();
+    if (!editEpisode.title || !editEpisode.duration || !editEpisode.language) {
+      alert("All fields are required!");
+      console.error("Missing required fields:", {
+        title: editEpisode.title,
+        duration: editEpisode.duration,
+        language: editEpisode.language,
+      });
+      return;
+    }
+
+    console.log("episode:", editEpisode);
+
+    try {
+      const episodeRef = doc(
+        db,
+        "series",
+        selectedSeries,
+        "episodes",
+        editEpisode.id
+      );
+      const updatedData = {
+        title: editEpisode.title,
+        duration: editEpisode.duration,
+        language: editEpisode.language,
+      };
+
+      console.log("Selected Series ID:", selectedSeries);
+      console.log("Episode ID:", editEpisode.id);
+
+      // If a new media file is uploaded
+      if (editEpisode.mediaFile && editEpisode.mediaFile.name) {
+        const mediaRef = ref(storage, `media/${editEpisode.mediaFile.name}`);
+        await uploadBytes(mediaRef, editEpisode.mediaFile);
+        const mediaUrl = await getDownloadURL(mediaRef);
+        updatedData.mediaUrl = mediaUrl;
+      } else if (editEpisode.mediaFile) {
+        console.error("Media file provided is invalid:", editEpisode.mediaFile);
+        alert("Invalid media file.");
+        return;
+      }
+
+      await updateDoc(episodeRef, updatedData);
+      alert("Episode updated successfully!");
+      setEditEpisode(null); // Clear edit mode
+      fetchSeries(); // Refresh data
+    } catch (error) {
+      console.error("Error updating episode:", error);
+      alert("Failed to update episode.");
+    }
   };
 
   useEffect(() => {
@@ -284,30 +366,128 @@ const Home = () => {
         <div className={styles.languageSidebar}>
           <h3>Select Language</h3>
           <ul>
-            {Object.keys(episodesBySeriesAndLanguage[selectedSeries] || {}).map((lang) => (
-              <li
-                key={lang}
-                className={selectedLanguage === lang ? styles.activeLanguage : ""}
-                onClick={() => setSelectedLanguage(lang)}
-              >
-                {lang}
-              </li>
-            ))}
+            {Object.keys(episodesBySeriesAndLanguage[selectedSeries] || {}).map(
+              (lang) => (
+                <li
+                  key={lang}
+                  className={
+                    selectedLanguage === lang ? styles.activeLanguage : ""
+                  }
+                  onClick={() => setSelectedLanguage(lang)}
+                >
+                  {lang}
+                </li>
+              )
+            )}
           </ul>
         </div>
 
         <div className={styles.episodeDisplay}>
           <h2>Episodes in {selectedLanguage || "Select a language"}</h2>
           <div className={styles.episodeContainer}>
-            {episodesBySeriesAndLanguage[selectedSeries]?.[selectedLanguage]?.length > 0 ? (
-              episodesBySeriesAndLanguage[selectedSeries][selectedLanguage].map((episode, index) => (
-                <div key={index}  className={styles.epCard}>
-                  <h3>{episode.title}</h3>
-                  <p>Duration: {episode.duration}</p>
-                  <p>Language: {episode.language}</p>
-                  <p>CreatedAt: {formatDate(episode.createdAt)}</p>
-                </div>
-              ))
+            {episodesBySeriesAndLanguage[selectedSeries]?.[selectedLanguage]
+              ?.length > 0 ? (
+              episodesBySeriesAndLanguage[selectedSeries][selectedLanguage].map(
+                (episode, index) => (
+                  <div key={index} className={styles.epCard}>
+                    <h3>{episode.title}</h3>
+                    <p>Duration: {episode.duration}</p>
+                    <p>Language: {episode.language}</p>
+                    <p>CreatedAt: {formatDate(episode.createdAt)}</p>
+                    <button
+                      onClick={() =>
+                        handleDeleteEpisode(selectedSeries, episode.id)
+                      }
+                      className={styles.deleteButton}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      onClick={() =>
+                        setEditEpisode({ ...episode, id: episode.id })
+                      }
+                      className={styles.editButton}
+                    >
+                      Edit
+                    </button>
+
+                    {editEpisode && editEpisode.id === episode.id && (
+                      <form
+                        onSubmit={handleUpdateEpisode}
+                        className={styles.form}
+                      >
+                        <input
+                          type="text"
+                          placeholder="Episode Title"
+                          value={editEpisode.title}
+                          onChange={(e) =>
+                            setEditEpisode({
+                              ...editEpisode,
+                              title: e.target.value,
+                            })
+                          }
+                          required
+                          className={styles.input}
+                        />
+                        <input
+                          type="text"
+                          placeholder="Duration (HH:MM:SS)"
+                          value={editEpisode.duration}
+                          onChange={(e) =>
+                            setEditEpisode({
+                              ...editEpisode,
+                              duration: e.target.value,
+                            })
+                          }
+                          required
+                          className={styles.input}
+                        />
+                        <input
+                          type="file"
+                          accept="audio/*,video/*"
+                          onChange={(e) => {
+                            if (e.target.files.length > 0) {
+                              setEditEpisode({
+                                ...editEpisode,
+                                mediaFile: e.target.files[0],
+                              });
+                            }
+                          }}
+                          className={styles.fileInput}
+                        />
+                        <select
+                          value={editEpisode.language}
+                          onChange={(e) =>
+                            setEditEpisode({
+                              ...editEpisode,
+                              language: e.target.value,
+                            })
+                          }
+                          className={styles.select}
+                          required
+                        >
+                          <option value="">Select Language</option>
+                          {languages.map((lang) => (
+                            <option key={lang} value={lang}>
+                              {lang}
+                            </option>
+                          ))}
+                        </select>
+                        <button type="submit" className={styles.button}>
+                          Update Episode
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => setEditEpisode(null)}
+                          className={styles.cancelButton}
+                        >
+                          Cancel
+                        </button>
+                      </form>
+                    )}
+                  </div>
+                )
+              )
             ) : (
               <p>No episodes available for this language and series.</p>
             )}
